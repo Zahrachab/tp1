94290a9ab0fd84d1eee401d1d56e1c1b46ec4a03
Zahrachab <fz_chabanechaouch@esi.dz>
2017-10-19 21:46:58 +0200

    new fichiers

---
+++ vectorHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPigl
+{
+    class vectorHelper
+    {
+        // les méthodes sont à modifier  
+        public void tri(int[] tab)
+        {
+
+        }
+        public void somme(int[] tab1, int[] tab2)
+        {
+
+        }
+        public void inverser(int[] tab)
+        {
+
+        }
+        public int[] min_max(int[] tab)
+        {
+            return tab;
+        }
+        public void applique_div(int[] tab)
+        {
+
+        }
+    }
+}
