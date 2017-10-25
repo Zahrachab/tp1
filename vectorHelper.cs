49696b83a2ee30db8c2475b09757926dad6be951
Hamitouche <32801614+Hamitouche@users.noreply.github.com>
2017-10-25 13:54:17 +0200

    Update vectorHelper.cs

--- vectorHelper.cs
+++ vectorHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +9,25 @@ namespace TPigl
     /// <summary>
     /// La classe vectorHelper qui permet 5 opérations
     /// </summary>
-    public static class vectorHelper
+
+
+    /// <include file='TPigl.XML' path='MyDocs/MyMembers[@name=" MainWindow"]/*' />
+    class vectorHelper
     {
         /// <summary>
         /// la méthode tri permet de trier le vecteur par ordre croissant
         /// </summary>
         /// <param name="tab"> le vecteur pour lequel on effectue le tri</param>
-        public static void tri(int[] tab)
+        /// <example> 
+        /// un ememple de tri d'un vecteur
+        /// <code> 
+        ///   vectorHelper vecHelp = new vectorHelper();
+        ///   int[] Vec = new int[]{1,8,5,3};
+        ///   vectorHelp.tri(Vec);
+        /// </code> 
+        /// </example>  
+
+        public void tri(int[] tab)
         {
             int i, j, x;
             for (i = 0; i < tab.Length; i++)
@@ -34,8 +46,21 @@ namespace TPigl
             /// <summary>
             /// la méthode somme permet de sommer deux vecteurs
             /// </summary>
+            /// <remarks>
+            ///Les deux vecteurs doivent etre de meme taille
+            ///</remarks>
             /// <param name="tab1"> le 1èr vecteur</param>
-            /// /// <param name="tab1"> le 2ème vecteur</param>
+            /// <param name="tab1"> le 2ème vecteur</param>
+            /// <example> 
+            /// un ememple de somme de deux vecteurs "Vec1" et "Vec2"
+           /// <code> 
+           /// vectorHelper vecHelp = new vectorHelper();
+           ///   int[] Vec1 = new int[]{1,8,5,3};
+           ///   int[] Vec2 =new int[]{1,8,5,3};
+           ///   vectorHelp.somme(Vec1,Vec2);
+           /// </code> 
+           /// </example> 
+      
       
         public static void somme(int[] tab1, int[] tab2) 
         {
@@ -55,12 +80,19 @@ namespace TPigl
 
         }
 
-
-        /// <summary>
+ /// <summary>
         /// la méthode inverser permet d'inverser le vecteur 
         /// </summary>
         /// <param name="tab"> le vecteur sur lequel on applique la méthode </param>
-        public static void inverser(int[] tab)
+        /// <example> 
+        /// un ememple d'inversion d'un vecteur  "Vec" 
+        /// <code> 
+        /// vectorHelper vecHelp = new vectorHelper();
+        ///   int[] Vec= int[]{1,8,5,3}; 
+        ///   vectorHelp.inverser(Vec1);
+        /// </code> 
+        /// </example> 
+        public void inverser(int[] tab)
         {
             int sauv = 0;
             int taille = tab.Length;
@@ -79,7 +111,16 @@ namespace TPigl
         /// </summary>
         /// <param name="tab"> le vecteur pour lequel le min et le max simultanément</param>
         /// <returns>retourne un tableau à 2 cases la première c'est le min et la deuxième c'est le max</returns>
-        public static int[] min_max(int[] tab)
+        ///     /// <example> 
+        /// un ememple qui fournit le min et le max d'un vecteur "Vec"
+        /// <code> 
+        /// vectorHelper vecHelp = new vectorHelper();
+        /// int[] result=new int[2];
+        ///   int[] Vec =new int[] {1,8,5,3};
+        ///   result=vecHelp.min_max(Vec);
+        /// </code> 
+        /// </example> 
+        public int[] min_max(int[] tab)
         {
             int max,min; 
             int[] min_max = new int[2];
@@ -99,7 +140,15 @@ namespace TPigl
         /// La méthode applique_div applique à tous les éléments d'un vecteur la fonction reste de la division par 3
         /// </summary>
         /// <param name="tab"> le paramètre tab est un vecteur auquel on applique la fonction reste de la division par 3</param>
-        public static void applique_div(int[] tab)
+        /// <example> 
+        /// un ememple d'application d'une division par 2 des elements d'un vecteur "Vec"
+        /// <code> 
+        /// vectorHelper vecHelp = new vectorHelper();
+        ///   int[] Vec= int[]{1,8,5,3}; 
+        ///   vectorHelp.applique_div(Vec);
+        /// </code> 
+        /// </example> 
+        public void applique_div(int[] tab)
         {
             for (int i = 0; i <tab.Length; i++)
             {
